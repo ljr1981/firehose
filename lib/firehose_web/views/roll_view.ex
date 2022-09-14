5978edcac0a4c1f16d09b1773fcc09a8c963a2d4
ljr1981 <ljr1981@msn.com>
2022-09-14 11:06:22 -0500

    comments

--- lib/firehose_web/views/roll_view.ex
+++ lib/firehose_web/views/roll_view.ex
@@ -1,6 +1,8 @@
 defmodule FirehoseWeb.RollView do
   use FirehoseWeb, :view
 
+  # Ref RollController def index
+  #
   def render("index.json", %{roll: num}) when is_integer(num) do
     %{status: "ok", roll: num}
   end
